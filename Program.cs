8b50c8c9efb6616431babc65d99cdf69a5625b80
Ravi Chande <rchande@microsoft.com>
2018-04-25 12:56:33 -0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace c73
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var x = (1, 2);
+            var y = (2, 3);
+            Console.WriteLine(x == y);
+        }
+    }
+}
